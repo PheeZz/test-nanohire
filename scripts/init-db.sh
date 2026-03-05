5eea9a3f4483f5cdb03d95e83aa9a5960b9a4809
pheezz <d.ya@qwerty.town>
2026-03-05 15:51:30 +0200

    feat: update database initialization script to include database name

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE nanohire_api;
-    GRANT ALL PRIVILEGES ON DATABASE nanohire_api TO postgres;
-EOSQL
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE nanohire_integration;
-    GRANT ALL PRIVILEGES ON DATABASE nanohire_integration TO postgres;
 EOSQL
-
-echo "Databases created successfully!"
